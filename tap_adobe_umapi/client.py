2e8472cfeb1d02df976ce100c1e1640f7d494242
Robby Robinson <robby.robinson@slalom.com>
2022-09-13 13:40:12 -0700

    added client

--- tap_adobe_umapi/client.py
+++ tap_adobe_umapi/client.py
@@ -14,21 +14,15 @@ from tap_adobe_umapi.auth import AdobeUmapiAuthenticator
 
 SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 
-
 class AdobeUmapiStream(RESTStream):
     """AdobeUmapi stream class."""
 
-    # TODO: Set the API's base URL here:
-    url_base = "https://api.mysample.com"
-
-    # OR use a dynamic url_base:
-    # @property
-    # def url_base(self) -> str:
-    #     """Return the API URL root, configurable via tap settings."""
-    #     return self.config["api_url"]
+    @property
+    def url_base(self) -> str:
+        """Return the API URL root, configurable via tap settings."""
+        return self.config.get("api_url")
 
     records_jsonpath = "$[*]"  # Or override `parse_response`.
-    next_page_token_jsonpath = "$.next_page"  # Or override `get_next_page_token`.
 
     @property
     @cached
@@ -39,57 +33,43 @@ class AdobeUmapiStream(RESTStream):
     @property
     def http_headers(self) -> dict:
         """Return the http headers needed."""
-        headers = {}
+
+        headers = {
+            "Content-type" : "application/json",
+            "Accept" : "application/json",
+            "x-api-key": self.config.get("api_key"),
+        }
+
         if "user_agent" in self.config:
             headers["User-Agent"] = self.config.get("user_agent")
+        
         return headers
 
-    def get_next_page_token(
-        self, response: requests.Response, previous_token: Optional[Any]
-    ) -> Optional[Any]:
-        """Return a token for identifying next page or None if no more pages."""
-        # TODO: If pagination is required, return a token which can be used to get the
-        #       next page. If this is the final page, return "None" to end the
-        #       pagination loop.
-        if self.next_page_token_jsonpath:
-            all_matches = extract_jsonpath(
-                self.next_page_token_jsonpath, response.json()
-            )
-            first_match = next(iter(all_matches), None)
-            next_page_token = first_match
-        else:
-            next_page_token = response.headers.get("X-Next-Page", None)
 
-        return next_page_token
+#    def get_next_page_token(
+#        self, response: requests.Response, previous_token: Optional[int]
+#    ) -> Optional[Any]:
+#        """Return a token for identifying next page or None if no more pages."""
+#        
+#        if previous_token:
+#            last_page: bool = response.json().get("lastPage")
+#
+#            if not last_page:
+#                next_page_token = 1 + previous_token
+#
+#        else:
+#            next_page_token = 0
+#
+#        return next_page_token
 
-    def get_url_params(
-        self, context: Optional[dict], next_page_token: Optional[Any]
-    ) -> Dict[str, Any]:
-        """Return a dictionary of values to be used in URL parameterization."""
-        params: dict = {}
-        if next_page_token:
-            params["page"] = next_page_token
-        if self.replication_key:
-            params["sort"] = "asc"
-            params["order_by"] = self.replication_key
-        return params
-
-    def prepare_request_payload(
-        self, context: Optional[dict], next_page_token: Optional[Any]
-    ) -> Optional[dict]:
-        """Prepare the data payload for the REST API request.
-
-        By default, no payload will be sent (return None).
-        """
-        # TODO: Delete this method if no payload is required. (Most REST APIs.)
-        return None
 
     def parse_response(self, response: requests.Response) -> Iterable[dict]:
         """Parse the response and return an iterator of result rows."""
         # TODO: Parse response body and return a set of records.
         yield from extract_jsonpath(self.records_jsonpath, input=response.json())
 
-    def post_process(self, row: dict, context: Optional[dict]) -> dict:
-        """As needed, append or transform raw data to match expected structure."""
-        # TODO: Delete this method if not needed.
-        return row
+        
+        # response_limit = 400
+        # limit of 25 requests per min
+        # response will give Retry-After: seconds
+        # if that is given, add a wait before continue
