ecb424295700a792c77adb5c60c1d824320da416
Robby Robinson <robby.robinson@slalom.com>
2022-10-27 17:35:33 -0700

    auth cleanup

--- tap_adobe_umapi/auth.py
+++ tap_adobe_umapi/auth.py
@@ -5,27 +5,24 @@ import time
 import jwt
 from typing import Any, Union
 
+IMS_HOST = "https://ims-na1.adobelogin.com"
+
 class AdobeUmapiAuthenticator(OAuthJWTAuthenticator):
     """Authenticator class for AdobeUmapi."""
-
-    ims_host = "https://ims-na1.adobelogin.com"
-
     @property
     def auth_endpoint(self) -> str:
-        return f'{self.ims_host}/ims/exchange/jwt'
+        return f'{IMS_HOST}/ims/exchange/jwt'
 
     @property
     def jwt(self) -> str:
         """Return JSON WEB Token for Adobe Developer Console OAuth requests
-
         Returns:
             JSON WEB Token for OAuth.
         """
-
         private_key: Union[bytes, Any] = bytes(self.private_key, "UTF-8")
         private_key_string: Union[str, Any] = private_key.decode("UTF-8")
-        api_key = self.config.get("api_key")
-        expiration = self.config.get("jwt_expiration")
+        api_key = self.config["api_key"]
+        expiration = self.config["jwt_expiration"]
         expiration_max = 60*60*24
 
         if expiration > expiration_max:
@@ -33,13 +30,13 @@ class AdobeUmapiAuthenticator(OAuthJWTAuthenticator):
 
         payload = {
             "exp": int(time.time()) + expiration,
-            "iss": self.config.get("organization_id"),
-            "sub": self.config.get("technical_account_id"),
-            "aud": f'{self.ims_host}/c/{api_key}'
+            "iss": self.config["organization_id"],
+            "sub": self.config["technical_account_id"],
+            "aud": f'{IMS_HOST}/c/{api_key}'
         }
 
         for scope in self.oauth_scopes:
-            scope_uri = f'{self.ims_host}/s/{scope}'
+            scope_uri = f'{IMS_HOST}/s/{scope}'
             payload[scope_uri] = True
 
         return jwt.encode(payload, private_key_string, "RS256")
@@ -51,9 +48,8 @@ class AdobeUmapiAuthenticator(OAuthJWTAuthenticator):
         Returns:
             Payload object for OAuth.
         """
-
         return {
-            "client_id": self.config.get("api_key"),
+            "client_id": self.config["api_key"],
             "client_secret": self.client_secret,
             "jwt_token": self.jwt
         }
