fa086ed9d3b215e792d7ad18d98f2aa2e0135b6e
Robby Robinson <robby.robinson@slalom.com>
2022-11-02 10:40:28 -0700

    formatting

--- tap_adobe_umapi/streams.py
+++ tap_adobe_umapi/streams.py
@@ -3,25 +3,25 @@
 from tap_adobe_umapi.client import AdobeUmapiStream
 from pathlib import Path
 
-SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
+SCHEMAS_DIR = Path(__file__).parent / Path('./schemas')
 
 
 class UsersStream(AdobeUmapiStream):
     """Define custom stream."""
-    name = "users"
-    path = "/users/{organization_id}/{page}"
-    records_jsonpath = "$.users[*]"
-    primary_keys = ["id"]
+    name = 'users'
+    path = '/users/{organization_id}/{page}'
+    records_jsonpath = '$.users[*]'
+    primary_keys = ['id']
     replication_key = None
     schema_filepath = f'{SCHEMAS_DIR}/users.json'
 
 
 class GroupsStream(AdobeUmapiStream):
     """Define custom stream."""
-    name = "groups"
-    path = "/groups/{organization_id}/{page}"
-    records_jsonpath = "$.groups[*]"
-    primary_keys = ["groupId"]
+    name = 'groups'
+    path = '/groups/{organization_id}/{page}'
+    records_jsonpath = '$.groups[*]'
+    primary_keys = ['groupId']
     replication_key = None
     schema_filepath = f'{SCHEMAS_DIR}/groups.json'
     
