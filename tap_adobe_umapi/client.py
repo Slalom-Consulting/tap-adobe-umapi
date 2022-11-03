25fe4217e7788d4a2b5e72dc5e7517871b294536
Robby Robinson <robby.robinson@slalom.com>
2022-11-02 18:25:47 -0700

    simplified pagination

--- tap_adobe_umapi/client.py
+++ tap_adobe_umapi/client.py
@@ -1,6 +1,6 @@
 """REST client handling, including AdobeUmapiStream base class."""
 
-from typing import Callable, Generator, Any
+from typing import Callable, Generator, Any, Optional
 from singer_sdk.streams import RESTStream
 from tap_adobe_umapi.auth import AdobeUmapiAuthenticator
 from tap_adobe_umapi.paginator import AdobeUmapiPaginator
@@ -54,35 +54,8 @@ class AdobeUmapiStream(RESTStream):
 
         return self.backoff_runtime(value=_backoff_from_headers)
 
-    def prepare_request(self, context, next_page_token) -> requests.PreparedRequest:
-        """Prepare a request object for this stream.
-        If partitioning is supported, the `context` object will contain the partition
-        definitions. Pagination information can be parsed from `next_page_token` if
-        `next_page_token` is not None.
-        Args:
-            context: Stream partition or context dictionary.
-            next_page_token: Token, page number or any request argument to request the
-                next page of data.
-        Returns:
-            Build a request with the stream's URL, path, query parameters,
-            HTTP headers and authenticator.
-        """
-        
-        if not context:
-            context = {}
-
+    def prepare_request(self, context: dict, next_page_token: Any) -> requests.PreparedRequest:
+        context = context or {}
         context['page'] = next_page_token or PAGINATION_INDEX
+        return super().prepare_request(context, None)
 
-        http_method = self.rest_method
-        url: str = self.get_url(context)
-        params: dict = self.get_url_params(context, None)
-        request_data = self.prepare_request_payload(context, None)
-        headers = self.http_headers
-
-        return self.build_prepared_request(
-           method=http_method,
-           url=url,
-           params=params,
-           headers=headers,
-           json=request_data,
-        )
