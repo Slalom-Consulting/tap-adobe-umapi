8d5a41f98a6be3f39a71034ebda1a1abf64947fa
Robby Robinson <robby.robinson@slalom.com>
2022-11-03 12:07:36 -0700

    sdk debugging code for vs code

--- tap_adobe_umapi/tap.py
+++ tap_adobe_umapi/tap.py
@@ -78,3 +78,7 @@ class TapAdobeUmapi(Tap):
     def discover_streams(self) -> List[Stream]:
         """Return a list of discovered streams."""
         return [stream_class(tap=self) for stream_class in STREAM_TYPES]
+
+
+if __name__ == "__main__":
+    TapAdobeUmapi.cli()
